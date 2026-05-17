fc7295e94c8eda981060d95dec1aa9a251ce53b7
nicolas kennedy <nico@mvpbuilders.tech>
2026-05-17 17:52:18 -0300

    Show today games on team pages

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,14 +2,34 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find_by!("LOWER(REPLACE(name, ' ', '-')) = ?", params[:id].downcase)
 
-    @upcoming_games = @team.games
-      .includes(:home_team, :away_team, :competition)
-      .where("starts_at > ?", Time.current)
+    argentina_zone = Time.find_zone!("America/Argentina/Buenos_Aires")
+    argentina_now = argentina_zone.now
+    today_range = argentina_now.beginning_of_day.utc..argentina_now.end_of_day.utc
+
+    base_games = @team.games.includes(:home_team, :away_team, :competition)
+
+    @today_games = base_games
+      .where(starts_at: today_range)
+      .order(
+        Arel.sql(
+          <<~SQL
+            CASE
+              WHEN games.status = 'live' THEN 0
+              WHEN games.status = 'scheduled' THEN 1
+              WHEN games.status = 'finished' THEN 2
+              ELSE 3
+            END,
+            games.starts_at ASC
+          SQL
+        )
+      )
+
+    @upcoming_games = base_games
+      .where("starts_at > ?", argentina_now.end_of_day.utc)
       .order(:starts_at)
 
-    @past_games = @team.games
-      .includes(:home_team, :away_team, :competition)
-      .where("starts_at <= ?", Time.current)
+    @past_games = base_games
+      .where("starts_at < ?", argentina_now.beginning_of_day.utc)
       .order(starts_at: :desc)
       .limit(10)
   end
