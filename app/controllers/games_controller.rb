aa409160557e01e26d85aec6283ad0d7ea7c160b
nicolas kennedy <nico@mvpbuilders.tech>
2026-05-17 17:27:24 -0300

    Improve games homepage prioritization

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,16 +11,26 @@ class GamesController < ApplicationController
       Team.none
     end
 
-    @competitions = Competition.order(
-      Arel.sql(
-        "CASE slug
-        WHEN 'world-cup-2026' THEN 1
-        WHEN 'libertadores' THEN 2
-        WHEN 'sudamericana' THEN 3
-        WHEN 'liga-argentina' THEN 4
-        ELSE 5 END"
+    allowed_slugs = [
+      "world-cup-2026",
+      "libertadores",
+      "sudamericana",
+      "liga-argentina",
+      "champions-league"
+    ]
+
+    @competitions = Competition
+      .where(slug: allowed_slugs)
+      .order(
+        Arel.sql(
+          "CASE slug
+          WHEN 'world-cup-2026' THEN 1
+          WHEN 'libertadores' THEN 2
+          WHEN 'sudamericana' THEN 3
+          WHEN 'liga-argentina' THEN 4
+          ELSE 5 END"
+        )
       )
-    )
     competition_slug = params[:competition].presence
 
     @competition = Competition.find_by(slug: competition_slug)
@@ -38,18 +48,53 @@ class GamesController < ApplicationController
         )
     end
 
-    @live_games = base_games.where(status: "live").order(:starts_at)
+    priority_competitions = [
+      "world-cup-2026",
+      "libertadores",
+      "sudamericana",
+      "liga-argentina",
+      "champions-league"
+    ]
+
+    priority_sql = <<~SQL
+      CASE
+        WHEN competitions.slug IN (#{priority_competitions.map { |s| "'#{s}'" }.join(",")})
+        THEN 0
+        ELSE 1
+      END
+    SQL
 
     today_range = argentina_now.beginning_of_day.utc..argentina_now.end_of_day.utc
 
-    @today_games = base_games
+    today_base = base_games
+      .joins(:competition)
       .where(starts_at: today_range)
-      .where.not(status: "live")
-      .order(:starts_at)
+
+    @today_games = today_base.order(
+      Arel.sql(
+        <<~SQL
+          CASE
+            WHEN games.status = 'live' THEN 0
+            WHEN games.status = 'scheduled' THEN 1
+            WHEN games.status = 'finished' THEN 2
+            ELSE 3
+          END,
+          #{priority_sql},
+          games.starts_at ASC
+        SQL
+      )
+    )
 
     @upcoming_games = base_games
-      .where("starts_at > ?", now)
-      .order(:starts_at)
+      .joins(:competition)
+      .where("starts_at > ?", now.end_of_day)
+      .order(
+        Arel.sql(
+          "#{priority_sql}, games.starts_at ASC"
+        )
+      )
+
+    @live_games = @today_games.select(&:live?)
   end
 
   def show
